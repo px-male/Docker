9f5884cad6e26a01fbaa620563309fdb55647350
px-male <36842437+px-male@users.noreply.github.com>
2020-07-16 22:32:58 +0900

    Update app.js

--- node.js_mysql/config/app.js
+++ node.js_mysql/config/app.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
+const connection = mysql.createConnection({
+  host: 'mys',
+  user: 'docker',
+  password: 'docker',
+  database: 'test_database'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
+
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  connection.query(
+    'SELECT * FROM users',
+    (error, results) => {
+      console.log(results);
+      res.render('hello.ejs');
+    }
+  );
 });
 
 app.listen(3000);
