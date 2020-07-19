5aa7e8abc25f594e177b07db77338de822b66980
px-male <36842437+px-male@users.noreply.github.com>
2020-07-19 22:50:08 +0900

    Update sampleapp.js

--- node.js/config/sampleapp.js
+++ node.js/config/sampleapp.js
@@ -1,13 +1,17 @@
 var http = require('http');
+var fs = require('fs');
  
 var server = http.createServer();
 server.on('request', doRequest);
-server.listen(3000);
+server.listen(8100);
 console.log('Server running!');
  
 // リクエストの処理
 function doRequest(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World\n');
-    res.end();
+    fs.readFile('./hello.html', 'UTF-8', 
+        function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
 }
